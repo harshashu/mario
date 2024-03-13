7502a67bc1c98d10bf7243f1e6cdcb2f1840fd67
harshashu <bodduharsha24@gmail.com>
2024-03-13 21:33:02 +0530

    updated

--- EKS-TF/backend.tf
+++ EKS-TF/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "harsha-mybook" # Replace with your actual S3 bucket name
+    bucket = "mario-mybucket" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
     region = "eu-north-1"
   }
